acdbc519d108d1ed134261765a270936bca3d5fd
Anh Nguyen <haianh1234>
2019-06-29 17:02:14 +0700

    .

---
+++ home.js
@@ -0,0 +1,9 @@
+var btnSearch = document.getElementById("btn_search");
+// btnSearch.addEventListener("click", function(e) {
+//     console.log(e);
+// });
+
+function search() {
+    console.log("Searched");
+}
+
